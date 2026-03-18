98990aa3cc8f231832af594acfc8530ff62626bb
Delta Student <krjitendra9430@gmail.com>
2026-03-18 16:36:00 +0530

    Auth API completed and MongoDB connected

--- server/server.js
+++ server/server.js
@@ -1,17 +1,27 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const authRoutes = require("./routes/authRoutes");
+
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("Chat app backend is runnig");
+app.use("/api/auth", authRoutes); 
+
+app.get("/", (req, res) => {
+  res.send("Chat App Backend Running");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server is runnig on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
